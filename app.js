eaa767c07c9b04a51526395aefb7db61ce09c926
Denis DAngelo <denis.dsilveira1@outlook.com>
2024-11-07 20:44:21 -0300

    quebrar lampada

--- app.js
+++ app.js
@@ -4,11 +4,11 @@
  */
 
 function quebrar(){
-    document.getElementById('lamp').src="img/broken.jpg"
     //reproduzindo um arquivo de audio no JS
     //passo 1: copiar o arquivo de audio para o projeto
     //passo 2: usar a classe Audio (biblioteca interna do JS)
     let som = new Audio()
     som.src = "sound/glassbreaking.wav"
     som.play()
+    document.getElementById('lamp').src="img/broken.jpg"
 }
