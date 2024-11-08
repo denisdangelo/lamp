11ce21ed291a820fd7e4b96b2220224c46e05145
Denis DAngelo <denis.dsilveira1@outlook.com>
2024-11-08 19:55:31 -0300

    correção da logica e adição de audio .mp3

--- app.js
+++ app.js
@@ -5,17 +5,23 @@
 let chave = false
 let lampada = true //a lampada esta ok
 
+//precarregamento do arquivo de audio
+let som = new Audio("sound/breaking-glass.mp3")
+    //som.src = "sound/glassbreaking.wav" - outra forma de chamar o som, para quando tiver mais de um audio
 
 
-function quebrar(){
-    //reproduzindo um arquivo de audio no JS
-    //passo 1: copiar o arquivo de audio para o projeto
-    //passo 2: usar a classe Audio (biblioteca interna do JS)
-    let som = new Audio()
-    som.src = "sound/glassbreaking.wav"
-    som.play()
-    document.getElementById('lamp').src="img/broken.jpg"
-    lampada = false //variavel lampada está quebrada
+function quebrar() {
+    if (lampada === true) {
+        document.getElementById('lamp').src = "img/broken.jpg"
+        //reproduzindo um arquivo de audio no JS
+        //passo 1: copiar o arquivo de audio para o projeto
+        //passo 2: usar a classe Audio (biblioteca interna do JS)
+        //passo 3: precarregar o arquivo de audio para sincronizar com a troca de imagem (UX)
+        
+        som.play()
+        
+        lampada = false //variavel lampada está quebrada
+    }
 }
 
 
@@ -23,21 +29,24 @@ function quebrar(){
 //variavel recebe o valor de false
 //a função recebe o if else
 // se o valor de chave for = à false ao clicar (está no html) na imagem
-function onoff(){
-    if (chave === false && lampada === true) {
-        document.getElementById('interruptor').src="img/swon.png"
+function onoff() {
+    
+
+    if (chave === false) {
+        document.getElementById('interruptor').src = "img/swon.png"
         chave = true //o JS agora sabe que a chave está ligada
-        document.getElementById('lamp').src="img/on.jpg" // muda a imagem da liuz para acesa
-    } else if (lampada === true){
-        document.getElementById('interruptor').src="img/swoff.png"
-        chave = false //o JS agora sabe que a chave está ligada
-        document.getElementById('lamp').src="img/off.jpg" // muda a imagem da luz para acesa
-    } else if (lampada === false && chave === false) {
-        document.getElementById('lamp').src="img/broken.jpg" //está errado
-        document.getElementById('lamp').src="img/off.jpg" // muda a imagem da luz para acesa
+
+        if (lampada === true) {
+            document.getElementById('lamp').src = "img/on.jpg" // muda a imagem da liuz para acesa
+        }
     } else {
-        document.getElementById('lamp').src="img/broken.jpg" //está errado
-        document.getElementById('interruptor').src="img/swon.png" 
+        document.getElementById('interruptor').src = "img/swoff.png"
+        chave = false //o JS agora sabe que a chave está ligada
+
+        if (lampada === true) {
+            document.getElementById('lamp').src = "img/off.jpg" // muda a imagem da luz para acesa
+        }
     }
-    }
+    
+}
 
